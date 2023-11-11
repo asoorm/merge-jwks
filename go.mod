2b483d95b48a46dcc10e2370f65ba5cd83c32915
asoorm <ahmet@mangomm.co.uk>
2023-11-11 00:20:00 +0000

    added example test and updated dependencies

--- go.mod
+++ go.mod
@@ -3,27 +3,34 @@ module github.com/asoorm/merge-jwks
 go 1.21
 
 require (
-	github.com/google/go-cmp v0.3.1
-	github.com/patrickmn/go-cache v2.1.0+incompatible
-	github.com/spf13/viper v1.6.3
-	github.com/square/go-jose v2.5.1+incompatible
+	github.com/google/go-cmp v0.6.0
+	github.com/spf13/viper v1.17.0
+	github.com/square/go-jose v2.6.0+incompatible
 )
 
 require (
-	github.com/fsnotify/fsnotify v1.4.7 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/magiconair/properties v1.8.1 // indirect
-	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.2.0 // indirect
-	github.com/spf13/afero v1.2.1 // indirect
-	github.com/spf13/cast v1.3.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/sagikazarmark/locafero v0.3.0 // indirect
+	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/spf13/afero v1.10.0 // indirect
+	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.0.0 // indirect
-	github.com/spf13/pflag v1.0.3 // indirect
-	github.com/subosito/gotenv v1.2.0 // indirect
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
-	golang.org/x/sys v0.0.0-20190804053845-51ab0e2deafa // indirect
-	golang.org/x/text v0.3.2 // indirect
-	gopkg.in/ini.v1 v1.51.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.6.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.9.0 // indirect
+	golang.org/x/crypto v0.13.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gopkg.in/yaml.v2 v2.2.4 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- merge_jwks_test.go
+++ merge_jwks_test.go
@@ -18,16 +18,16 @@ type testStruct struct {
 var testcases = []testStruct{
 	{
 		In: []byte(`{
-  "keys": [
-    {
-      "kid": "L_OIax5OnVSaZL0Rkikdo6_4z7CttymiBxGhdizUQP0",
-      "kty": "RSA",
-      "alg": "RS256",
-      "use": "sig",
-      "n": "tQDpW46x1Zjnwrqu-PcpPPxPTxVYryRwNbpkSbkC1i46mmvhI-zHfCSd2fdAhsqNX6XtzUNF10vOd0rR1U8jNxPMXYV_kHD7pOdJsc2kdDS6uRT9AIg9WHe0AoK2HraPEyAnVgq5TWVxx0IT4YGDXupOniAHNPcZ0dPNlxV5VdD8lsKXBOs6HWA93UqwxF6pYiEthxzE4kPZQaB6s6qQ5RGs47wYISyw-cUdDHp5VH_wJIr4Y9Vi8S-vEsKC9_XqQwvqMBQ96WRMDReoreUEsXPR3AX7_yE7h7UgHFV_qSNyLGFoNTTwioc7A5-S6OEuxc7MGZp-XBWsVuMEuXg0vQ",
-      "e": "AQAB"
-    }
-  ]
+ "keys": [
+   {
+     "kid": "L_OIax5OnVSaZL0Rkikdo6_4z7CttymiBxGhdizUQP0",
+     "kty": "RSA",
+     "alg": "RS256",
+     "use": "sig",
+     "n": "tQDpW46x1Zjnwrqu-PcpPPxPTxVYryRwNbpkSbkC1i46mmvhI-zHfCSd2fdAhsqNX6XtzUNF10vOd0rR1U8jNxPMXYV_kHD7pOdJsc2kdDS6uRT9AIg9WHe0AoK2HraPEyAnVgq5TWVxx0IT4YGDXupOniAHNPcZ0dPNlxV5VdD8lsKXBOs6HWA93UqwxF6pYiEthxzE4kPZQaB6s6qQ5RGs47wYISyw-cUdDHp5VH_wJIr4Y9Vi8S-vEsKC9_XqQwvqMBQ96WRMDReoreUEsXPR3AX7_yE7h7UgHFV_qSNyLGFoNTTwioc7A5-S6OEuxc7MGZp-XBWsVuMEuXg0vQ",
+     "e": "AQAB"
+   }
+ ]
 }`),
 		Out: jwksUriResponse{
 			Keys: []jwksTmpl{
@@ -81,16 +81,16 @@ var testcases = []testStruct{
 	},
 	{
 		In: []byte(`{
-  "keys": [
-    {
-      "kty": "RSA",
-      "alg": "RS256",
-      "kid": "V1y7y0M7B6rdA0MXWgPj6bYEb3Md2mXULcU_IvL2URM",
-      "use": "sig",
-      "e": "AQAB",
-      "n": "nZPtrhV6ernRxuPI-Sz6kdFRXJR0CFx03UmBHzh0gDpwYsudnY-0AIxxVYAf3kDAhS9qhjVsVio-W7A3IXPlCmuQfDGmpc5vlUfIPgTAuAxt9zY3udk8dNdWY68YzFGSLpuQfCV8uIpwJJxapNjJn5VkVEEh2-b0t-JDPqcO023-0y-mxp4v7UV5Ddv-YfOtxbAKYlKwiConORpuQD-g-is_FZynm4mxSKbb59MKtwIfcjxllDafwNOq4g3TZBXTnqM42I-RpEwyIV5TGaZ2jEJVm9VpXQEgUW2wPDIPfyY1Ie3FrRfMSzTL8efhW74Wa6wPj2njPUqT6-16v0XBBQ"
-    }
-  ]
+ "keys": [
+   {
+     "kty": "RSA",
+     "alg": "RS256",
+     "kid": "V1y7y0M7B6rdA0MXWgPj6bYEb3Md2mXULcU_IvL2URM",
+     "use": "sig",
+     "e": "AQAB",
+     "n": "nZPtrhV6ernRxuPI-Sz6kdFRXJR0CFx03UmBHzh0gDpwYsudnY-0AIxxVYAf3kDAhS9qhjVsVio-W7A3IXPlCmuQfDGmpc5vlUfIPgTAuAxt9zY3udk8dNdWY68YzFGSLpuQfCV8uIpwJJxapNjJn5VkVEEh2-b0t-JDPqcO023-0y-mxp4v7UV5Ddv-YfOtxbAKYlKwiConORpuQD-g-is_FZynm4mxSKbb59MKtwIfcjxllDafwNOq4g3TZBXTnqM42I-RpEwyIV5TGaZ2jEJVm9VpXQEgUW2wPDIPfyY1Ie3FrRfMSzTL8efhW74Wa6wPj2njPUqT6-16v0XBBQ"
+   }
+ ]
 }`),
 		Out: jwksUriResponse{
 			Keys: []jwksTmpl{
@@ -103,6 +103,38 @@ var testcases = []testStruct{
 					E:   "AQAB",
 					X5C: []string{
 						"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnZPtrhV6ernRxuPI+Sz6kdFRXJR0CFx03UmBHzh0gDpwYsudnY+0AIxxVYAf3kDAhS9qhjVsVio+W7A3IXPlCmuQfDGmpc5vlUfIPgTAuAxt9zY3udk8dNdWY68YzFGSLpuQfCV8uIpwJJxapNjJn5VkVEEh2+b0t+JDPqcO023+0y+mxp4v7UV5Ddv+YfOtxbAKYlKwiConORpuQD+g+is/FZynm4mxSKbb59MKtwIfcjxllDafwNOq4g3TZBXTnqM42I+RpEwyIV5TGaZ2jEJVm9VpXQEgUW2wPDIPfyY1Ie3FrRfMSzTL8efhW74Wa6wPj2njPUqT6+16v0XBBQIDAQAB",
+					},
+				},
+			},
+		},
+	},
+	{
+		In: []byte(`{
+"keys": [{
+"kid": "AKkEX5alnDUxP3RevOrdya_st2ZP4-Am90zHyfDavzs",
+"kty": "RSA",
+"alg": "RS256",
+"use": "sig",
+"n": "yLjokLQJrWw8jWwThYonnKP0__qEyTTOCUy35JNCr83akvv5bINbaQmshm8po4jKDsxP29LAy5kkMVzT_f-5TovUuHDxbfc4U2DglTzQbppYNddcQlRR3gTu7FR_oe6fzyrkbOfEY9e_Rchgx4dosqThWic1-T49W3exaieV6qnZVQbW7LTZ8IaP9LTFrn4Lw00towM3XKrANp9CoJuGF8u1CL5a_-gShCyZIX1noqIbzZZVG2_atT9OkGKR-sjwXP_BIK5RhDv4BZZXYxoSJcqoihG1NX7SxqQ4zsYIzK3YVLbRNOvDi-YworXTgKx-T5Mygz6P_XvCs3NMRlqWKw",
+"e": "AQAB",
+"x5c": [
+"MIICmzCCAYMCBgGCo0hNUjANBgkqhkiG9w0BAQsFADARMQ8wDQYDVQQDDAZtYXN0ZXIwHhcNMjIwODE1MjA1MTQwWhcNMzIwODE1MjA1MzIwWjARMQ8wDQYDVQQDDAZtYXN0ZXIwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDIuOiQtAmtbDyNbBOFiieco/T/+oTJNM4JTLfkk0KvzdqS+/lsg1tpCayGbymjiMoOzE/b0sDLmSQxXNP9/7lOi9S4cPFt9zhTYOCVPNBumlg111xCVFHeBO7sVH+h7p/PKuRs58Rj179FyGDHh2iypOFaJzX5Pj1bd7FqJ5XqqdlVBtbstNnwho/0tMWufgvDTS2jAzdcqsA2n0Kgm4YXy7UIvlr/6BKELJkhfWeiohvNllUbb9q1P06QYpH6yPBc/8EgrlGEO/gFlldjGhIlyqiKEbU1ftLGpDjOxgjMrdhUttE068OL5jCitdOArH5PkzKDPo/9e8Kzc0xGWpYrAgMBAAEwDQYJKoZIhvcNAQELBQADggEBAG88l1wJXR/oGR9mA2EzTmTJ+taqoBh4phpAUgIONHVZ4Az5DAy66ZyKC3pdA6R0+AJxlmVkAV4m/+qdx9dDQm32jrC6YXcvdHaBbU3mIdcjFBkkTvnAn7I+IjWFRMLjVlTV2quziPs4jyoyTwpXiTs1yG9/MFIR086mm9wcxPW4crcJKPaiZ6r4kd0g1hHXkQrjiyUZUy9MPqn0rydpDAqk+y5k/iB81E3vPPc0rKtr6z+CAZeikAAabBjEv9vb5vPBsF4YG1XZGr99gJMf14muGc9afeV5f6sLY3C/sqN/JcKHY5jx3qE+0MnQKsyVaSZpjEYT76Ye+7NhP3vVp1U="
+],
+"x5t": "yws7kBzoOkiOZBE63tiGTiSilNo",
+"x5t#S256": "A_Uv7s5uxWHGaA1J3sDx_uCghTDAKyXExzyW69iNEzA"
+}]
+}`),
+		Out: jwksUriResponse{
+			Keys: []jwksTmpl{
+				{
+					Kid: "AKkEX5alnDUxP3RevOrdya_st2ZP4-Am90zHyfDavzs",
+					Kty: "RSA",
+					Alg: "RS256",
+					Use: "sig",
+					N:   "yLjokLQJrWw8jWwThYonnKP0__qEyTTOCUy35JNCr83akvv5bINbaQmshm8po4jKDsxP29LAy5kkMVzT_f-5TovUuHDxbfc4U2DglTzQbppYNddcQlRR3gTu7FR_oe6fzyrkbOfEY9e_Rchgx4dosqThWic1-T49W3exaieV6qnZVQbW7LTZ8IaP9LTFrn4Lw00towM3XKrANp9CoJuGF8u1CL5a_-gShCyZIX1noqIbzZZVG2_atT9OkGKR-sjwXP_BIK5RhDv4BZZXYxoSJcqoihG1NX7SxqQ4zsYIzK3YVLbRNOvDi-YworXTgKx-T5Mygz6P_XvCs3NMRlqWKw",
+					E:   "AQAB",
+					X5C: []string{
+						"MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyLjokLQJrWw8jWwThYonnKP0//qEyTTOCUy35JNCr83akvv5bINbaQmshm8po4jKDsxP29LAy5kkMVzT/f+5TovUuHDxbfc4U2DglTzQbppYNddcQlRR3gTu7FR/oe6fzyrkbOfEY9e/Rchgx4dosqThWic1+T49W3exaieV6qnZVQbW7LTZ8IaP9LTFrn4Lw00towM3XKrANp9CoJuGF8u1CL5a/+gShCyZIX1noqIbzZZVG2/atT9OkGKR+sjwXP/BIK5RhDv4BZZXYxoSJcqoihG1NX7SxqQ4zsYIzK3YVLbRNOvDi+YworXTgKx+T5Mygz6P/XvCs3NMRlqWKwIDAQAB",
 					},
 				},
 			},
@@ -131,6 +163,8 @@ func TestTranslateJWKSet(t *testing.T) {
 
 				t.Log("exp: ", string(testOutBytes))
 				t.Log("got: ", string(newJwksBytes))
+
+				t.Logf("paste RSA key into https://keytool.online to validate\n\n-----BEGIN PUBLIC KEY-----\n%s\n-----END PUBLIC KEY-----\n\n", translated[0].X5C[0])
 
 				t.FailNow()
 			}
