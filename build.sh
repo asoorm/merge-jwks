9c64a698fc1de64fa56ab5422eb9053a10e87cfe
asoorm <ahmet@mangomm.co.uk>
2023-11-11 01:12:39 +0000

    adding whitespace

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ echo $VERSION
 git tag ${VERSION}
 git push -f --tags
 docker buildx build --no-cache --push -t mangomm/merge-jwks:${VERSION} --platform=linux/amd64 --build-arg version=${VERSION} .
+
 docker scout cves local://mangomm/merge-jwks:${VERSION}
